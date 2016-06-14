89ec31fa327b8e7936f43ad3710c3f38a510b395
Rajat Gupta <rajat310198@outlook.com>
2016-06-14 23:08:28 +0530

    Update main.py

--- Python/RandomSelect/main.py
+++ Python/RandomSelect/main.py
@@ -1,15 +1,28 @@
 import os
 import sys
 import random
-import level
+
+from level import Level
+from player import Player
 
 print(sys.platform)
 
 def getRandom(rang):
 	return random.randint(0,rang)
 
+
+print("1.New Player\n2.Load game\n3.Exit")
+i = int(input())
+
+if i==1:
+	name = input("Name :")
+if i==2:
+	name = input("Name :")
+pl = Player(name)
+
 t = 1
-rang = level.Level()
+#rang = level.Level()
+rang = Level()
 score = 0
 while t and rang.getLevel() <20:
 	i = int(input('Enter no.:'))
@@ -26,3 +39,8 @@ if rang==20:
 
 else:
 	print("Loose\nScore is",score)
+
+
+pl.modifyPlayer(name,score)
+
+del pl
