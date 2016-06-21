9d16f632d6f471bb152e756ed354c01348c85d74
Rajat Gupta <rajat310198@outlook.com>
2016-06-21 21:37:27 +0530

    Update linkedlist.h

--- DSA/List/linkedlist.h
+++ DSA/List/linkedlist.h
@@ -16,6 +16,32 @@ class LinkedList
 public:
     node<T> *head;
     int count;
+    LinkedList();
+    void insert(T n);
+    void insertEnd(T n);
+    void insertAfter(T n,T key);
+    void deleteB();
+    void deleteEnd();
+    void deleteAny(T key);
+    void display();
+    int size();
+    #ifndef LINKED_LIST_H
+#define LINKED_LIST_H
+
+ #include <iostream>
+using namespace std;
+template <class T>
+class node{
+    public:
+     T item;
+     node<T> *next;
+};
+template <class T>
+class LinkedList
+{
+public:
+    node<T> *head;
+    int count;
 	LinkedList();
 	void insert(T n);
     void insertEnd(T n);
@@ -25,6 +51,24 @@ public:
     void deleteAny(T key);
     void display();
     int size();
+    friend ostream &operator<<(ostream &out,const LinkedList<T> &L)
+    {
+       
+      	node<T>*t = L.head;
+      	while(t)
+      	{
+      		out<<t->item<<" ";
+      		t=t->next;
+      	}
+
+    }
 	~LinkedList();
+
+};
+
+#include "linkedlist.hpp"
+#endif
+
+    ~LinkedList();
 
 };
