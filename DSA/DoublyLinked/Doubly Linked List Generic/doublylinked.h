c6a7511e8c888a47bd7cb53f5502e552cbc86b24
Rajat Gupta <rajat310198@outlook.com>
2016-06-21 21:42:01 +0530

    Update doublylinked.h

--- DSA/DoublyLinked/Doubly Linked List Generic/doublylinked.h
+++ DSA/DoublyLinked/Doubly Linked List Generic/doublylinked.h
@@ -1,8 +1,8 @@
 #ifndef DOUBLY_LINKED_H
-#define DOUBLY_LINKED_H 
+#define DOUBLY_LINKED_H
 #include <iostream>
 template<class T>
-class node{                 
+class node{
     public:
      T item;
      node<T> *next,*prev;
@@ -11,19 +11,28 @@ template<class T>
 class DoublyLinked
 {
 public:
-	 node<T> *head;
-	DoublyLinked();
-	void insertB(T item);
+	  node<T> *head;
+	  DoublyLinked();
+	  void insertB(T item);
     void insertE(T item);
-	void insertAny(T item,T key);
+	  void insertAny(T item,T key);
     void deleteB();
     void deleteE();
     void deleteAny(T key);
     void display();
     int count();
-   // bool search();
-	//~DoublyLinked();
-	
+    friend ostream &operator<<(ostream &out,const LinkedList<T> &L)
+    {
+       
+      	node<T>*t = L.head;
+      	while(t)
+      	{
+      		out<<t->item<<" ";
+      		t=t->next;
+      	}
+
+    }
+
 };
 #include "doublylinked.hpp"
 #endif
